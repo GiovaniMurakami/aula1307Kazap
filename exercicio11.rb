f53669666d213ac52536938894dc91581f109f90
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-13 20:45:02 -0300

    Adicionado solução exercício 11

---
+++ exercicio11.rb
@@ -0,0 +1,8 @@
+#11- Implemente um método que receba uma string como parâmetro e retorne a mesma
+#string invertida.
+
+def inverter(string)
+    return string.reverse
+end
+
+puts inverter("Giovani")
