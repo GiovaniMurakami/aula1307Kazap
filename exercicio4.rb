ea3857ea946f044bbb3e5e9d0f48b83f087130e8
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-13 20:18:41 -0300

    Adicionado solução exercicio 4

---
+++ exercicio4.rb
@@ -0,0 +1,9 @@
+#4- Implemente um método que receba um nome como parâmetro e retorne uma saudação personalizada.
+
+nome = 'Giovani'
+
+def saudacao(string)
+    puts "Olá, #{string}"
+end
+
+saudacao(nome)
