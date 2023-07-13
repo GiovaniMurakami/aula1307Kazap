40e1ed73638939ade9ab968c3926e573fd25c9df
giovaniMurakami <giovani.murakami@outlook.com>
2023-07-13 20:48:41 -0300

    Adicionado solução exercício 13

---
+++ exercicio13.rb
@@ -0,0 +1,8 @@
+#13- Crie um método que receba dois números como parâmetros e retorne a potência do
+#primeiro número elevado ao segundo número.
+
+def potencia(num, num2)
+    return num ** num2
+end
+
+puts potencia(2, 10)
